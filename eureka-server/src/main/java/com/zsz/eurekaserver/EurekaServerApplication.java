5ebc0ead0b201b7bb925dfc25e69f83bba342210
朱顺忠 <zhushunzhong@chaoxing.com>
2018-08-03 17:15:39 +0800

    测试修改

--- eureka-server/src/main/java/com/zsz/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/zsz/eurekaserver/EurekaServerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableEurekaServer
 @RestController
+
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
