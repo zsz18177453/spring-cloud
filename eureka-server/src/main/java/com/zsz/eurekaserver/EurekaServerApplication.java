d889f4af5e27ce4c71b0c5bdeed8437dcdf5583d
朱顺忠 <zhushunzhong@chaoxing.com>
2018-08-03 17:14:17 +0800

    测试修改

--- eureka-server/src/main/java/com/zsz/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/zsz/eurekaserver/EurekaServerApplication.java
@@ -3,9 +3,11 @@ package com.zsz.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaServer
+@RestController
 public class EurekaServerApplication {
 
 	public static void main(String[] args) {
